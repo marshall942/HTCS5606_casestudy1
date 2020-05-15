4bf97f9b9f07105f9462ae893b68db62c2c377fe
marshall942 <isabel.pitcher@yahoo.com>
2020-05-15 23:43:20 +1200

    e

--- js/script.js
+++ js/script.js
@@ -14,4 +14,48 @@ function carousel() {
 }
 setTimeout (carousel, 3000);
 
+//above is the javascript animation
+
+
+
+//array for products - "name", "price"
+// var items = [["Dog food brand 1", "16.49"], ["Dog food brand 2", "5.99"],
+//     ["Cat food brand 1", "14.99"], ["Cat food brand 2", "12.99"],
+//     ["Bird food brand 1", "33.49"], ["Bird food brand 2", "12.99"],
+//     ["Reptile food brand 1", "2.49"], ["Reptile food brand 2", "11.99"]];
+
+
+
 //
+// function invoicePrice() {
+//     var price = document.getElementsByClassName("productPrice")
+//     price = parseFloat(productPrice.value)
+//     var result=
+// }
+//
+// function add() {
+//     num1Input = document.getElementById("num1Input");
+//     num2Input = document.getElementById("num2Input");
+//     num1 = parseFloat(num1Input.value);
+//     num2 = parseFloat(num2Input.value);
+//     var result = num1+num2;
+//     resultPara = document.getElementById("showCalculationResult");
+//     resultPara.innerText = result;
+// }
+//
+//
+// function subtract() {
+//     num1Input = document.getElementById("num1Input");
+//     num2Input = document.getElementById("num2Input");
+//     num1 = parseFloat(num1Input.value);
+//     num2 = parseFloat(num2Input.value);
+//     var result = num1-num2;
+//     resultPara = document.getElementById("showCalculationResult");
+//     resultPara.innerText = result;
+// }
+//
+//
+// function discount() {
+//     if invoicePrice >= 300
+//     invoicePrice =
+// }
